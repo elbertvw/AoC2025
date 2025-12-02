6e874fb416224c9ba3c5fee91173928c3e559ac5
elbert <elbert@debian-BULLSEYE-live-builder-AMD64>
2025-12-02 21:35:33 +0100

    Small refactor of 01

--- 01/01_part_one.py
+++ 01/01_part_one.py
@@ -9,12 +9,11 @@ def determine_new_position(starting_position, line):
 
 
 with open('./input') as input_file:
-    current_position = STARTING_POSITION
-    tally_zeroes = 0
+    position = STARTING_POSITION
+    total_zeroes = 0
 
-    for current_line in input_file:
-        current_position = determine_new_position(current_position, current_line)
-        if current_position == 0:
-            tally_zeroes += 1
+    for line in input_file:
+        position = determine_new_position(position, line)
+        total_zeroes += (position == 0)
 
-    print(tally_zeroes)
+    print(total_zeroes)
--- 01/01_part_two.py
+++ 01/01_part_two.py
@@ -4,27 +4,28 @@ DIAL_SIZE = 100
 def determine_new_position(starting_position, line):
     dial_distance = int(line[1:])
     if line[0] == 'L':
-        return (starting_position - dial_distance) % DIAL_SIZE
+        dial_distance = -dial_distance
     return (starting_position + dial_distance) % DIAL_SIZE
 
 
 def determine_amount_of_zeroes_passed(starting_position, line):
     dial_distance = int(line[1:])
     if line[0] == 'L':
+        full_dial_turns = dial_distance // DIAL_SIZE
         remainder = dial_distance % DIAL_SIZE
-        return dial_distance // DIAL_SIZE + (starting_position != 0 and remainder >= starting_position)
+        optional_extra_turn = (starting_position != 0 and remainder >= starting_position)
+        return full_dial_turns + optional_extra_turn
     else:
         return (starting_position + dial_distance) // DIAL_SIZE
 
 
 with open('./input') as input_file:
-    current_position = STARTING_POSITION
-    tally_zeroes = 0
+    position = STARTING_POSITION
+    total_zeroes = 0
 
-    for current_line in input_file:
-        initial_position = current_position
-        amount_of_zeroes_passed = determine_amount_of_zeroes_passed(current_position, current_line)
-        current_position = determine_new_position(current_position, current_line)
-        tally_zeroes += amount_of_zeroes_passed
+    for line in input_file:
+        zeroes_passed = determine_amount_of_zeroes_passed(position, line)
+        position = determine_new_position(position, line)
+        total_zeroes += zeroes_passed
 
-    print(tally_zeroes)
+    print(total_zeroes)
