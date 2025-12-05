f3df4957e139e48a43a77b79b62c6e6dd3fc78c5
elbert <elbert@debian-BULLSEYE-live-builder-AMD64>
2025-12-05 12:05:26 +0100

    Solution for day 4

---
+++ 04/04_part_one.py
@@ -0,0 +1,36 @@
+INPUT_FILENAME = 'input'
+PLACEHOLDER = '?'
+ROLL = '@'
+EMPTY = '.'
+MAX_ADJACENT_ROLLS = 4
+
+
+def check_if_removable(lines, x, y):
+    top, bottom = max(0, y - 1), min(len(lines), y + 2) # hacky way of taking into account the edges of the lines matrix
+    left, right = max(0, x - 1), min(len(lines[y]), x + 2) # + 1 and + 2, because range() is x inclusive to y exclusive
+
+    count = 0
+    for yy in range(top, bottom):
+        for xx in range(left, right):
+            if lines[yy][xx] == ROLL: # the coord being checked is included, be aware that it too will be counted!
+                count += 1
+                if count > MAX_ADJACENT_ROLLS:
+                    return False
+    return True
+
+
+def solve(input):
+    lines = input.split('\n')
+    result = 0
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            if char == ROLL:
+                result += check_if_removable(lines, x, y)
+    return result
+
+
+with open(INPUT_FILENAME) as input_file:
+    print(solve(input_file.read()))
+
+# test case
+# print(solve(TEST))  # = 13
---
+++ 04/04_part_two.py
@@ -0,0 +1,43 @@
+INPUT_FILENAME = 'input'
+PLACEHOLDER = '?'
+ROLL = '@'
+EMPTY = '.'
+MAX_ADJACENT_ROLLS = 4
+
+
+def check_if_removable(lines, x, y):
+    top, bottom = max(0, y - 1), min(len(lines), y + 2) # hacky way of taking into account the edges of the lines matrix
+    left, right = max(0, x - 1), min(len(lines[y]), x + 2) # + 1 and + 2, because range() is x inclusive to y exclusive
+
+    count = 0
+    for yy in range(top, bottom):
+        for xx in range(left, right):
+            if lines[yy][xx] == ROLL: # the coord being checked is included, be aware that it too will be counted!
+                count += 1
+                if count > MAX_ADJACENT_ROLLS:
+                    return False
+    return True
+
+
+def solve(puzzle):
+    lines = [list(line) for line in puzzle.split('\n')] # convenience: lists are mutable, strings are not
+    solution = 0
+
+    while True:
+        intermediate_result = 0
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                if char == ROLL:
+                    is_removable = check_if_removable(lines, x, y)
+                    if is_removable:
+                        lines[y][x] = EMPTY
+                        intermediate_result += 1
+        if intermediate_result == 0:
+            break
+        solution += intermediate_result
+
+    return solution
+
+
+with open(INPUT_FILENAME) as input_file:
+    print(solve(input_file.read()))
