e3879acf8638eabc7b471bc956c4530a30b9e593
elbertvw <78381289+elbertvw@users.noreply.github.com>
2025-12-06 23:06:22 +0100

    Slight refactor of 6.2

--- 06/06_part_two.py
+++ 06/06_part_two.py
@@ -2,6 +2,10 @@ from functools import reduce
 from operator import mul
 
 INPUT_FILENAME = 'input'
+OPERATIONS = {
+    '+': lambda x: sum(x),
+    '*': lambda x: reduce(mul, x)
+}
 
 with open(INPUT_FILENAME) as input_file:
     lines = [line.strip() for line in input_file.readlines()]
@@ -15,13 +19,14 @@ with open(INPUT_FILENAME) as input_file:
     
     for column in range(width):
         column_content = ''.join(line[column] for line in lines[:-1] if line[column] != ' ')
-        if column_content: numbers.append(int(''.join([n for n in column_content])))
-
-        if lines[-1][column] in '+*': operator = lines[-1][column]
-        if not column_content or column == final_column:
-            if operator == '+': solution += sum(numbers)
-            if operator == '*': solution += reduce(mul, numbers)
+        if column_content: 
+            numbers.append(int(''.join([n for n in column_content])))
+            
+        if lines[-1][column] in OPERATIONS: 
+            operator = lines[-1][column]
+            
+        if not column_content or column == final_column: 
+            solution += OPERATIONS[operator](numbers)
             numbers = []
                 
 print(solution) 
-
