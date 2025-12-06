db92ce1389d2175392c63ed4f4b02cd54c5bdaf9
elbertvw <78381289+elbertvw@users.noreply.github.com>
2025-12-06 22:31:00 +0100

    Simplify 06_part_two; no need to iterate in reverse

--- 06/06_part_two.py
+++ 06/06_part_two.py
@@ -3,39 +3,25 @@ from operator import mul
 
 INPUT_FILENAME = 'input'
 
-def calculate(numbers, operator):
-    return sum(numbers) if operator == '+' else reduce(mul, numbers)
-
-
-# Join characters in each column, strip spaces, remove empty columns
-def clean_columns(columns):
-    return [''.join(col).replace(' ', '') for col in columns if ''.join(col).strip()]
-
-
-# If we reverse the input, we can use the operator as a delimiter separating problems 
 with open(INPUT_FILENAME) as input_file:
-    lines = [line[::-1] for line in input_file]
-    height = len(lines)
-    width = max(len(line) for line in lines)
+    lines = [line.strip() for line in input_file.readlines()]
+    width = max(len(line) for line in lines) 
+    lines = [line.ljust(width) for line in lines]
+    final_column = width - 1 
     
-    problems = []
-    problem_columns = [[] for _ in range(height)]
-    problem_column = 0 
-
+    solution = 0
+    numbers = []
+    operator = ''
+    
     for column in range(width):
-        end_of_current_problem = False
+        column_content = ''.join(line[column] for line in lines[:-1] if line[column] != ' ')
+        if column_content: numbers.append(int(''.join([n for n in column_content])))
 
-        for line in lines:
-            char = line[column]
-            if char in '+*': end_of_current_problem = True
-            else: problem_columns[problem_column].append(char)
-
-        if end_of_current_problem:
-            numbers = [int(n) for n in clean_columns(problem_columns)]
-            problems.append((numbers, char))
-            problem_columns = [[] for _ in range(height)]
-            problem_column = 0
-        else:
-            problem_column += 1
-
-print(sum(calculate(numbers, op) for numbers, op in problems))
+        if lines[-1][column] in '+*': operator = lines[-1][column]
+        if not column_content or column == final_column:
+            if operator == '+': solution += sum(numbers)
+            if operator == '*': solution += reduce(mul, numbers)
+            operator = ''
+            numbers = []
+                
+print(solution) 
