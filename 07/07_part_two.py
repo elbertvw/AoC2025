a450834cff4d103c3527a4ca9e3bbf7801ac08e2
elbertvw <78381289+elbertvw@users.noreply.github.com>
2025-12-07 17:47:54 +0100

    Slight simplification of 7.2

--- 07/07_part_two.py
+++ 07/07_part_two.py
@@ -4,19 +4,16 @@ INPUT_FILENAME = 'input'
 START_CHAR = 'S'
 SPLIT_CHAR = '^'
 
-
 @lru_cache(None)
-def sum_possible_paths(start_x_y, lines):
-    x, y = start_x_y
+def sum_possible_paths(x, y, lines):
     if y == len(lines):
         return 1
     elif lines[y][x] == SPLIT_CHAR:
-        return sum_possible_paths((x + 1, y + 1,), lines) + sum_possible_paths((x - 1, y + 1), lines)
+        return sum_possible_paths(x + 1, y + 1, lines) + sum_possible_paths(x - 1, y + 1, lines)
     else:
-        return sum_possible_paths((x, y + 1), lines)
-
+        return sum_possible_paths(x, y + 1, lines)
 
 with (open(INPUT_FILENAME) as file):
     lines = file.readlines()        
     start_x = lines[0].index(START_CHAR)
-    print(sum_possible_paths((start_x, 0), tuple(lines))) # tuple is hashable for lru_cache purposes  
+    print(sum_possible_paths(start_x, 0, tuple(lines))) # tuple is hashable for lru_cache purposes  
