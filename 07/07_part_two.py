4f005868adb3192121bbbf042652301b62912ac9
elbertvw <78381289+elbertvw@users.noreply.github.com>
2025-12-07 18:02:46 +0100

    7.2: rm unnecessary parentheses

--- 07/07_part_two.py
+++ 07/07_part_two.py
@@ -13,7 +13,7 @@ def sum_possible_paths(x, y, lines):
     else:
         return sum_possible_paths(x, y + 1, lines)
 
-with (open(INPUT_FILENAME) as file):
+with open(INPUT_FILENAME) as file:
     lines = file.readlines()        
     start_x = lines[0].index(START_CHAR)
     print(sum_possible_paths(start_x, 0, tuple(lines))) # tuple is hashable for lru_cache purposes  
